467d9a1046686b44558b0f3ed58814ac16889419
Bowen <shibowen01@baidu.com>
2020-08-31 11:45:17 +0800

    2020/08/28

---
+++ JS/17. 电话号码的字母组合/letterCombinations.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function(digits) {
+    if (!digits) return [];
+
+    const charMap = {
+        '2': ['a', 'b', 'c'],
+        '3': ['d', 'e', 'f'],
+        '4': ['g', 'h', 'i'],
+        '5': ['j', 'k', 'l'],
+        '6': ['m', 'n', 'o'],
+        '7': ['p', 'q', 'r', 's'],
+        '8': ['t', 'u', 'v'],
+        '9': ['w', 'x', 'y', 'z']
+    }
+    const l = digits.length;
+    
+    // 次慢
+    const inputChar = (i, cur) => {
+        if (i >= l) return cur;
+        
+        let t = []
+        let s;
+        while(cur.length){
+            let s = cur.pop()
+            t.push(...charMap[digits[i]].map(c => s + c))
+        }
+        return inputChar(
+            i + 1,
+            t
+        )
+    };
+    return inputChar(0, [""]);
+};
