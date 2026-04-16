a820d1439b6ddef740619932ad5f0cc6dc2bf6d1
Bowen <bowenandbugs@gmail.com>
2026-04-15 20:14:42 -0700

    Apr 15 2026

--- Python/755. Pour Water/main.py
+++ Python/755. Pour Water/main.py
@@ -2,6 +2,129 @@
 
 from typing import List
 
+# time: O(v + n), space: O(n)
+class Solution:
+    def pourWater(self, heights: List[int], volume: int, k: int) -> List[int]:
+        n = len(heights)
+        l = k
+        r = k
+        baseHeight = heights[k]
+        INF_HEIGHT = 3000 # use 3000 to represent Infinity, because max height would be less than 2100
+
+        def fillLeftGap():
+            nonlocal heights, volume, l, baseHeight
+            if l == -1:
+                return
+            lStack = [l]
+            while True:
+                l -= 1
+                curHeight = INF_HEIGHT if l == -1 else heights[l]
+                if curHeight < heights[lStack[-1]]:
+                    lStack.append(l)
+                elif curHeight == heights[lStack[-1]]:
+                    lStack[-1] = l
+                else:
+                    while curHeight > heights[lStack[-1]]:
+                        if len(lStack) <= 1:
+                            return
+                        
+                        waterBottom = heights[lStack.pop()]
+                        targetHeight = min(curHeight, heights[lStack[-1]])
+                        waterHeight = targetHeight - waterBottom
+                        waterWidth = lStack[-1] - l - 1
+                        waterNeeded = waterWidth * waterHeight
+
+                        if volume >= waterNeeded:
+                            volume -= waterNeeded
+                            for i in range(l + 1, lStack[-1]):
+                                heights[i] = targetHeight
+                        else:
+                            lowerHeight = volume // waterWidth + waterBottom
+                            higherCount = volume % waterWidth
+                            for i in range(l + 1, lStack[-1] - higherCount):
+                                heights[i] = lowerHeight
+                            for i in range(lStack[-1] - higherCount, lStack[-1]):
+                                heights[i] = lowerHeight + 1
+                            volume = 0
+                            return
+                        
+                    if curHeight < heights[lStack[-1]]:
+                        lStack.append(l)
+                    elif curHeight == heights[lStack[-1]]:
+                        lStack[-1] = l
+        
+        def fillRightGap():
+            nonlocal heights, volume, r, baseHeight
+            if r == n:
+                return
+            rStack = [r]
+            while True:
+                r += 1
+                curHeight = INF_HEIGHT if r == n else heights[r]
+                if curHeight < heights[rStack[-1]]:
+                    rStack.append(r)
+                elif curHeight == heights[rStack[-1]]:
+                    rStack[-1] = r
+                else:
+                    while curHeight > heights[rStack[-1]]:
+                        if len(rStack) <= 1:
+                            return
+                        
+                        waterBottom = heights[rStack.pop()]
+                        targetHeight = min(curHeight, heights[rStack[-1]])
+                        waterHeight = targetHeight - waterBottom
+                        waterWidth = r - rStack[-1] - 1
+                        waterNeeded = waterWidth * waterHeight
+
+                        if volume >= waterNeeded:
+                            volume -= waterNeeded
+                            for i in range(rStack[-1] + 1, r):
+                                heights[i] = targetHeight
+                        else:
+                            lowerHeight = volume // waterWidth + waterBottom
+                            higherCount = volume % waterWidth
+                            for i in range(rStack[-1] + 1, rStack[-1] + 1 + higherCount):
+                                heights[i] = lowerHeight + 1
+                            for i in range(rStack[-1] + 1 + higherCount, r):
+                                heights[i] = lowerHeight
+                            volume = 0
+                            return
+                        
+                    if curHeight < heights[rStack[-1]]:
+                        rStack.append(r)
+                    if curHeight == heights[rStack[-1]]:
+                        rStack[-1] = r
+
+        while True:
+            # First, fill left
+            lastL = l
+            if l > -1 and heights[l] <= baseHeight:
+                fillLeftGap()
+                print(f"l filled from {lastL} to {l}")
+                print(heights, volume)
+                if volume == 0:
+                    return heights
+            
+            # left full, fill right
+            lastR = r
+            if lastL == l and r < n and heights[r] <= baseHeight:
+                fillRightGap()
+                print(f"r filled from {lastR} to {r}")
+                print(heights, volume)
+                if volume == 0:
+                    return heights
+            
+            # left full, right full, add middleHeight
+            if lastL == l and lastR == r:
+                volume -= 1
+                baseHeight += 1
+                heights[k] = baseHeight
+                l = k
+                r = k
+                print(heights, volume)
+                if volume == 0:
+                    return heights
+
 
 # I thought if a drop of water can slide to the right when it is [1,2,3,4,4,4,3,2,1]
 #                                                                           ⬆
